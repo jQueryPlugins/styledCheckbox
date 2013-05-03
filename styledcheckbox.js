39733935f88ecfc247d28ab0f815cf913497cfaa
Netanel Edri <netanelweb@gmail.com>
2013-05-04 01:14:17 +0300

    first version of js file before debugging

--- styledcheckbox.js
+++ styledcheckbox.js
@@ -9,6 +9,8 @@
     defaults = {
         checkedCls : 'styledCheckbox-checked',
         uncheckedCls : 'styledCheckbox-unchecked',
+        hideCls : 'styledCheckbox-hide',
+        wrapperCls : 'styledCheckbox-control',
         onChange : function(){}
     };
 
@@ -22,7 +24,41 @@
 
     Plugin.prototype = {
         init: function(){
+            var that = this , el = that.element , options = that.options;
 
+            that.log('start init','info');
+
+            var wrapperTag = '<div class="' + options.wrapperCls + '"></div>';
+            
+            if(el.attr('type') === 'checkbox' && el.is(':checkbox')){
+
+                el.addClass(options.hideCls).wrap(wrapperTag).change(function(){
+
+                    that.log('input change checked mode','info');
+
+                    var wrapper = el.parent();
+
+                    if(el.is(':checked')){
+                        wrapper.removeClass(options.uncheckedCls).addClass(options.checkedCls);
+                    } else {
+                        wrapper.removeClass(options.checkedCls).addClass(options.uncheckedCls);
+                    }
+
+                });
+
+                if(el.is(':checked')){
+                    that.log('input is already checked so add the checked class','info');
+                    el.parent().addClass(options.checkedCls);
+                }
+
+            } else {
+                that.log('The element is not a checkbox','error');
+            }
+
+            that.log('finish init','info');
+        },
+        log : function(msg,type){
+            console.log('@styledCheckboxPlugin ' + type + ' : ' + msg);
         }
     };
 
